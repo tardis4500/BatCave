fc550e06e34e39f4b9988a013816e196b4da23e8
Jeff <web@pobox.com>
2019-11-17 21:43:12 -0500

    Update docstrings for automation module. (GitLab #29.)

--- batcave/automation.py
+++ batcave/automation.py
@@ -11,31 +11,55 @@ from .sysutil import popd, SysCmdRunner
 class ActionCommandRunner(SysCmdRunner):
     """Class to wrap SysCmdRunner for simple usage with auto-logging."""
 
-    def __init__(self, command, guard='', default_args=tuple(), **keys):
-        super().__init__(command, *default_args, show_cmd=True, show_stdout=True, **keys)
+    def __init__(self, command: str, guard: str = '', default_args: tuple = tuple(), **kwargs):
+        """
+        Args:
+            command: The command passed to SysCmdRunner.
+            guard: A line to be printed before the command is run.
+                If an empty string (the default), nothing is printed.
+            default_args: The list of default args passed to SysCmdRunner.
+            **kwargs: The list of default named args passed to SysCmdRunner.
+        """
+        super().__init__(command, *default_args, show_cmd=True, show_stdout=True, **kwargs)
         self.guard = guard
 
-    def run(self, message, *args, **keys):
+    def run(self, message: str, *args, **kwargs):
+        """
+        Args:
+            message: The message passed to SysCmdRunner.
+            *args: The list of args passed to SysCmdRunner.
+            **kwargs: The list of named args passed to SysCmdRunner.
+
+        Returns:
+            Returns whatever is returned by SysCmdRunner
+        """
         if self.guard:
             self.writer(self.guard)
-        return super().run(message, *args, **keys)
+        return super().run(message, *args, **kwargs)
 
 
 class Action:
     """The common base class for all actions.
-       This is a virtual class and the inheriting class must at least include a _execute() method
-       The action is invoked by calling the execute() method this will run the following methods:
-            pre()
-            _execute()
-            post()
-       These are run in a try to catch any exceptions after which
-            always_post()
-       is run
+
+    This is a virtual class and the inheriting class must at least include a _execute() method
+    The action is invoked by calling the execute() method which will run the following methods:
+        pre()
+        _execute()
+        post()
+    These are run in a try to catch any exceptions after which
+        always_post()
+    is run.
+
+    Attributes:
+        MESSAGE_GUARD: This string is printed by logger if the value of guard passed to logger is true.
     """
+    MESSAGE_GUARD: str = f"{'*'*70}"
 
-    MESSAGE_GUARD = f"{'*'*70}"
-
-    def __init__(self, **_unused_args):
+    def __init__(self, **_unused_kwargs):
+        """
+        Args:
+            **_unused_kwargs: All arguments passed are ignored by the time they reach this initializer
+        """
         self.project_root = Path.cwd().parent
 
     def __enter__(self):
@@ -45,17 +69,17 @@ class Action:
         return False
 
     def pre(self):
-        'Executed before _execute()'
+        """Executed before _execute()."""
 
     def post(self):
-        'Executed after _execute()'
+        """Executed after _execute()."""
 
     def always_post(self):
-        'Executed after _execute() like finally in try/catch/finally'
+        """Executed after _execute() like finally in try/catch/finally."""
         popd()
 
     def execute(self):
-        'Runs the _execute() method from the child class'
+        """Runs the _execute() method from the child class."""
         try:
             self.pre()
             self._execute()  # pylint: disable=E1101
@@ -63,8 +87,18 @@ class Action:
         finally:
             self.always_post()
 
-    def log_message(self, message, guard=False, leader='INFO'):
-        'Logs a message to stdout and flushes the stream'
+    def log_message(self, message: str, guard: bool = False, leader: str = 'INFO'):
+        """Logs a message to stdout and flushes the stream
+
+        Args:
+            message: The message to be printed.
+            guard: If True, MESSAGE_GUARD will be printed on a line before the message.
+            leader: If it does not evaluate to False, it will be prepended to every printed
+                line, including the guard.
+
+        Returns:
+            Nothing
+        """
         guard = self.MESSAGE_GUARD if (guard is True) else ''
         leader = f'{leader} ' if leader else ''
         if guard:
