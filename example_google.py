0caa996b0a61eb45787f3c89ece6be4a928b9888
Jeff <web@pobox.com>
2020-03-05 22:04:51 -0500

    Update TODO list.

--- example_google.py
+++ example_google.py
@@ -8,7 +8,7 @@
 # Step4: Add Error docstrings.
 # Step5: Add class __init__ docstrings. Arguments, Attributes, Raises.
 # Step6: Add class property docstrings.
-# Step7: Add class method docstrings. Next: iispy
+# Step7: Add class method docstrings. Next: iispy (then continue k8s)
 
 # Started adding class method docstrings for the k8s module. (GitLab #29)
 
