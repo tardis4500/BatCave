866cc8d9a2a4978159603db0e947986ffc18e7fe
Jeff <web@pobox.com>
2020-04-26 19:52:28 -0400

    Added PEP484 type hints to automation module. (GitLab #29)

--- batcave/automation.py
+++ batcave/automation.py
@@ -2,7 +2,9 @@
 
 # Import standard modules
 import sys
+from abc import abstractmethod
 from pathlib import Path
+from typing import Any, List
 
 # Import internal modules
 from .sysutil import popd, SysCmdRunner
@@ -11,7 +13,7 @@ from .sysutil import popd, SysCmdRunner
 class ActionCommandRunner(SysCmdRunner):
     """Class to wrap SysCmdRunner for simple usage with auto-logging."""
 
-    def __init__(self, command: str, guard: str = '', default_args: tuple = tuple(), **kwargs):
+    def __init__(self, command: str, guard: str = '', default_args: tuple = tuple(), **kwargs: Any):
         """
         Args:
             command: The command passed to SysCmdRunner.
@@ -26,7 +28,7 @@ class ActionCommandRunner(SysCmdRunner):
         super().__init__(command, *default_args, show_cmd=True, show_stdout=True, **kwargs)
         self.guard = guard
 
-    def run(self, message: str, *args, **kwargs):
+    def run(self, message: str, *args: Any, **kwargs: Any) -> List[str]:
         """Run the action.
 
         Args:
@@ -59,7 +61,7 @@ class Action:
     """
     MESSAGE_GUARD: str = f"{'*'*70}"
 
-    def __init__(self, **_unused_kwargs):
+    def __init__(self, **_unused_kwargs: Any):
         """
         Args:
             **_unused_kwargs: All arguments passed are ignored by the time they reach this initializer.
@@ -72,24 +74,28 @@ class Action:
     def __enter__(self):
         return self
 
-    def __exit__(self, *exc_info):
+    def __exit__(self, *exc_info: Any):
         return False
 
-    def pre(self):
+    @abstractmethod
+    def _execute(self) -> None:
+        pass
+
+    def pre(self) -> None:
         """Executed before _execute().
 
         Returns:
             Nothing.
         """
 
-    def post(self):
+    def post(self) -> None:
         """Executed after _execute().
 
         Returns:
             Nothing.
         """
 
-    def always_post(self):
+    def always_post(self) -> None:
         """Always executed after _execute() as in finally in try/catch/finally.
 
         Returns:
@@ -97,7 +103,7 @@ class Action:
         """
         popd()
 
-    def execute(self):
+    def execute(self) -> None:
         """Run the _execute() method from the child class.
 
         Returns:
@@ -105,12 +111,12 @@ class Action:
         """
         try:
             self.pre()
-            self._execute()  # pylint: disable=E1101
+            self._execute()
             self.post()
         finally:
             self.always_post()
 
-    def log_message(self, message: str, guard: bool = False, leader: str = 'INFO'):
+    def log_message(self, message: str, guard: bool = False, leader: str = 'INFO') -> None:
         """Log a message to stdout and flushes the stream.
 
         Args:
@@ -122,9 +128,9 @@ class Action:
         Returns:
             Nothing.
         """
-        guard = self.MESSAGE_GUARD if (guard is True) else ''
+        the_guard = self.MESSAGE_GUARD if (guard is True) else ''
         leader = f'{leader} ' if leader else ''
-        if guard:
-            print(f'{leader}{guard}')
+        if the_guard:
+            print(f'{leader}{the_guard}')
         print(f'{leader}{message}')
         sys.stdout.flush()
--- example_google.py
+++ example_google.py
@@ -11,10 +11,10 @@
 # Step7: Add class method docstrings.
 # Step8: Add class private method docstrings.
 # Step9: Reorder Classes: private, protected, properties, methods.
-
-# Reordered class members alphabetically by type. (GitLab #29)
-
 # Step10: Add PEP484 type hints.
+
+# Added PEP484 type hints to automation module. (GitLab #29)
+
 # Step11: Add PEP526 variable type annotations
 # Step12: Add linting and type checking to build pipeline.
 
