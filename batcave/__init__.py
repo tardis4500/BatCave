918b0c8ad35981ff61b365d3a0063a60626f7d68
Jeff <web@pobox.com>
2023-05-29 22:38:52 -0400

    bump version 42.0.0rc10 -> 42.0.0rc11

--- batcave/__init__.py
+++ batcave/__init__.py
@@ -44,7 +44,7 @@ __title__ = 'BatCave'
 __summary__ = 'Python Programming Toolkit'
 __uri__ = 'https://gitlab.com/arisilon/batcave/'
 
-__version__ = '42.0.0rc10'
+__version__ = '42.0.0rc11'
 __build_name__ = '{var:build_name}'
 __build_date__ = '{var:build_date}'
 
