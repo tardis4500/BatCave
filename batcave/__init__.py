bd74d92783a9be8ed9ff70ca7df5f5ca463e052f
Jeff Smith <tardis4500@gmail.com>
2023-06-14 20:55:30 +0000

    bump version 42.0.2 -> 42.0.3rc0 [skip-ci]

--- batcave/__init__.py
+++ batcave/__init__.py
@@ -44,7 +44,7 @@ __title__ = 'BatCave'
 __summary__ = 'Python Programming Toolkit'
 __uri__ = 'https://gitlab.com/arisilon/batcave/'
 
-__version__ = '42.0.2'
+__version__ = '42.0.3rc0'
 __build_name__ = '{var:build_name}'
 __build_date__ = '{var:build_date}'
 
