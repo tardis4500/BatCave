17a85276d7d6896fb694a564614cc7a195972fd6
Jeff <web@pobox.com>
2024-01-17 22:14:55 -0500

    Implement publish-test.

--- cicd-support/cicd.sh
+++ cicd-support/cicd.sh
@@ -14,23 +14,30 @@ pip install --upgrade --upgrade-strategy eager setuptools wheel
 # source $VIRTUAL_ENV/bin/activate
 # pip install --upgrade --upgrade-strategy eager pip
 # pip install --upgrade --upgrade-strategy eager setuptools wheel
+# git remote add $GIT_REMOTE https://${{ github.actor }}:$GITLAB_USER_TOKEN@${{ github.server_url }}/${{ github.repository_owner }}/${{ github.event.repository.name }}.git
 
-if [ $1 != install-test ]
-then
+function install-flit {
     pip install --upgrade --upgrade-strategy eager flit
     flit install -s --deps all
-fi
-# git remote add $GIT_REMOTE https://${{ github.actor }}:$GITLAB_USER_TOKEN@${{ github.server_url }}/${{ github.repository_owner }}/${{ github.event.repository.name }}.git
+}
 
 case $1 in
     static-analysis )
+        install-flit
         pylint $PRODUCT
         flake8 $PRODUCT
         mypy $PRODUCT ;;
     unit-tests )
+        install-flit
         python -m xmlrunner discover -o $UNIT_TEST_DIR ;;
     build )
+        install-flit
         flit build ;;
     install-test )
         pip install $ARTIFACTS_DIR/*.tar.gz ;;
+    publish-test )
+        twine upload --config-file $PYPIRC -r $PYPI_REPO $ARTIFACTS_DIR/*
+        bumpver update $BUMP_ARGS
 esac
+
+# cSpell:ignore virtualenv mypy xmlrunner pypirc pypi bumpver
