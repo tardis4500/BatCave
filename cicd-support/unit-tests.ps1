f9e68300f2088f37568d3852c82dc6ec0bbb078f
Jeff Smith <jeff.smith@forescout.com>
2024-01-31 16:16:54 -0500

    Implement Windows unit tests.

---
+++ cicd-support/unit-tests.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+
+Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+$PSVersionTable
+python --version
+pip install --upgrade --upgrade-strategy eager pip
+pip install --upgrade --upgrade-strategy eager setuptools wheel
+pip install --upgrade --upgrade-strategy eager flit
+flit install -s --deps all
+python -m xmlrunner discover -o $env:UNIT_TEST_DIR
+
+# cSpell:ignore hklm xmlrunner
