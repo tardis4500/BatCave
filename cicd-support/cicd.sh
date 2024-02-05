a464ab2e29b1bdf54594a115eef9b4be07d5eefd
Jeff <web@pobox.com>
2024-02-05 08:37:38 -0500

    Fix publish.

--- cicd-support/cicd.sh
+++ cicd-support/cicd.sh
@@ -4,7 +4,6 @@ set -eu
 PRODUCT=batcave
 export FLIT_ROOT_INSTALL=1
 
-
 unix_os=`uname`
 if [ $unix_os = Darwin ]
 then
@@ -17,14 +16,15 @@ else
     exit 1
 fi
 
-function install-flit {
-    pip install --upgrade --upgrade-strategy eager flit
-    flit install -s --deps all
-}
-
 function install-pip-tools {
     pip install --upgrade --upgrade-strategy eager pip
     pip install --upgrade --upgrade-strategy eager setuptools wheel
+}
+
+function run-bumpver {
+    git config user.name "$GIT_AUTHOR_NAME"
+    git config user.email "$GIT_AUTHOR_EMAIL"
+    bumpver $*
 }
 
 python --version
@@ -35,32 +35,31 @@ if [ ! -e $VIRTUAL_ENV ]; then virtualenv $VIRTUAL_ENV; fi
 source $VIRTUAL_ENV/bin/activate
 install-pip-tools
 
-git config user.name "$GIT_AUTHOR_NAME"
-git config user.email "$GIT_AUTHOR_EMAIL"
+if [ $1 != "install-test"]
+then
+    pip install --upgrade --upgrade-strategy eager flit
+    flit install -s --deps all
+fi
+
 case $1 in
     static-analysis )
-        install-flit
         pylint $PRODUCT
         flake8 $PRODUCT
         mypy $PRODUCT ;;
     unit-tests )
-        install-flit
         python -m xmlrunner discover -o $UNIT_TEST_DIR ;;
     build )
-        install-flit
         flit build ;;
     install-test )
         pip install $ARTIFACTS_DIR/*.tar.gz ;;
     publish-test )
-        install-flit
-        bumpver update --tag-num ;;
+        run-bumpver update --tag-num ;;
     publish )
-        install-flit
-        bumpver update --tag final --tag-commit
+        run-bumpver update --tag final --tag-commit
         flit build
         eval $(bumpver show --env)
         gh release create $CURRENT_VERSION --title="Release $CURRENT_VERSION" --latest --generate-notes
-        bumpver update --patch --tag rc --tag-num ;;
+        run-bumpver update --patch --tag rc --tag-num ;;
 esac
 
 # cSpell:ignore virtualenv mypy xmlrunner bumpver batcave
