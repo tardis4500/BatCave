25e0de494b059e9b2256c798dfc0b1fda11bc000
Jeff <web@pobox.com>
2024-05-17 22:48:49 -0400

    Update utility scripts.

--- util/New-Env.ps1
+++ util/New-Env.ps1
@@ -2,10 +2,10 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
-mkvirtualenv vjer
+mkvirtualenv batcave
 python -m pip install --upgrade pip
 pip install --upgrade --upgrade-strategy eager setuptools wheel
 pip install --upgrade --upgrade-strategy eager flit
 flit install --deps all
 
-# cSpell:ignore mkvirtualenv vjer
+# cSpell:ignore mkvirtualenv batcave
--- util/new-env.sh
+++ util/new-env.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 
-mkvirtualenv vjer
+mkvirtualenv batcave
 python -m pip install --upgrade pip
 pip install --upgrade --upgrade-strategy eager setuptools wheel
 pip install --upgrade --upgrade-strategy eager flit
 flit install -s --deps all
 
-# cSpell:ignore mkvirtualenv vjer
+# cSpell:ignore mkvirtualenv batcave
