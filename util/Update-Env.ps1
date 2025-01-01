8220d4ed36fc250d215f00c6771db4b5992f0ca6
Jeff <web@pobox.com>
2025-01-01 16:12:46 -0500

    Update support scripts. [skip ci]

--- util/Update-Env.ps1
+++ util/Update-Env.ps1
@@ -3,5 +3,7 @@ Set-StrictMode -Version Latest
 
 python -m pip install --upgrade pip
 pip install --upgrade --upgrade-strategy eager setuptools wheel
-pip freeze | %{$_.split('==')[0]} | %{pip install --upgrade $_}
+pip install --upgrade --upgrade-strategy eager flit
+pip install --upgrade --upgrade-strategy eager .
+pip freeze | ForEach-Object{$_.split('==')[0]} | ForEach-Object{pip install --upgrade $_}
 flit install --deps all
--- util/update-env.sh
+++ util/update-env.sh
@@ -3,5 +3,7 @@ set -eu
 
 python -m pip install --upgrade pip
 pip install --upgrade --upgrade-strategy eager setuptools wheel
+pip install --upgrade --upgrade-strategy eager flit
+pip install --upgrade --upgrade-strategy eager .
 pip freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install --upgrade
 flit install -s --deps all
